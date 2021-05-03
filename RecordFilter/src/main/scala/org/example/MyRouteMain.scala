f8fa324ab2314b02725c130f0a4aac778549ab86
rahuldubey1789 <43521769+rahuldubey1789@users.noreply.github.com>
2021-05-03 18:01:30 +0530

    Add files via upload
    Replacing file with File stream and using stream output.

--- RecordFilter/src/main/scala/org/example/MyRouteMain.scala
+++ RecordFilter/src/main/scala/org/example/MyRouteMain.scala
@@ -3,7 +3,7 @@ package org.example
 import org.apache.spark.sql.{Dataset, SparkSession}
 import org.apache.spark.sql.functions.{col, concat, date_format, explode, expr, from_json, from_unixtime, lit, split, to_timestamp, unix_timestamp}
 import org.example.schema.RecordClass
-import org.apache.spark.sql.types.{IntegerType, MapType, StringType}
+import org.apache.spark.sql.types.{IntegerType, MapType, StringType, StructField, StructType}
 /**
  * A Main to run MyRouteBuilder
  */
@@ -11,8 +11,8 @@ object MyRouteMain {
 
   def main(args: Array[String]) {
     if(args.length == 0 ){
-   throw new IllegalArgumentException("Argument not specified")
-   }
+     throw new IllegalArgumentException("Argument not specified")
+    }
     new Processor(args).start()
   }
 
@@ -21,30 +21,41 @@ object MyRouteMain {
 class Processor (args: Array[String]){
 
   def start(): Unit = {
-  val inputFile =  args(0)
-  val outputFile = args(1)
+    val inputFile =  args(0)
+    val outputFile = args(1)
     val spark = SparkSession.builder.master("local[*]").appName("FilterRecord").getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
     import spark.implicits._
-  var inputDf = spark.read.text(inputFile)
-    val mapSchema = MapType(StringType, StringType)
-  //val df = inputDf.map(row => row.getString(0).split("\"message\": ")(1)).map
-    inputDf = inputDf.withColumn("message", from_json($"value", mapSchema).getItem("message")).select("message").filter("message is not null")
+
+    val schema = StructType(
+      List(
+        StructField("message", StringType, true)))
+
+    var inputDf = spark.readStream
+      .schema(schema)
+      .json(inputFile)
+
+    inputDf = inputDf.filter(col("message").contains("omwssu"))
     val replacedDf = inputDf.map(row => row.getString(0).replaceAll("\\s+"," "))
-    //print(replacedDf.show())
     var df = replacedDf.withColumn("tmp", split($"value", " "))
     df = df.withColumn("date",$"tmp".getItem(1)).withColumn("time",$"tmp".getItem(2)).withColumn("url",$"tmp".getItem(12))
-    df = df.drop("value").drop("tmp")
-    df= df.filter(col("url").contains("omwssu"))
+    df = df.drop("tmp")
     df = df.withColumn("datetime",concat(col("date"), lit(" ") ,col("time")))
     df = df.withColumn("times2",to_timestamp($"datetime","yyyy-MM-dd HH:mm:ss")).withColumn("timestamp", from_unixtime(unix_timestamp(col("times2"), "yyyy-MM-dd HH:mm:ss"),"yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'"))
     df = df.drop("date","time","datetime","times2")
-    
+
+    //details from the URL can be extracted using regex as below
     val urlPattern = "^(\\w+):\\/{2}(\\w*)\\.?([^\\/]*)([^\\?]*)\\??(/errors/)?".r
     val egUrl = "http://omwssu.lab5a.nl.dmdsdp.com/errors/0000F0-HZNSTB-123456789/devupdate/0/5/Testing%20by%20Selene%20Team"
     urlPattern.findAllIn(egUrl).matchData.foreach{x=> println(s"${x.group(1)} ${x.group(2)} ${x.group(3)} ${x.group(4)} ${x.group(5)}")}
-    print(df.show())
-   // print(df.select("tmp").first())
-    //print(df.select("date").first())
+
+    df.writeStream
+      .format("json")
+      .option("path", outputFile)
+      .option("checkpointLocation", "D:\\New_folder\\checkpoint")
+      .outputMode("append")
+      .start().awaitTermination()
+
 
   }
 
